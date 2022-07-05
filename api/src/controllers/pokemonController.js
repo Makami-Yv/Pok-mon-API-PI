894046978f08cef4eff37f364222f7d960037b97
Makami-Yv <attercoppe@protonmail.com>
2022-07-05 13:43:05 -0500

    Controller and routes second version, maybe needs a refactor

--- api/src/controllers/pokemonController.js
+++ api/src/controllers/pokemonController.js
@@ -1,39 +1,104 @@
 const { Pokemon, Type, Op } = require('../db.js');
 const axios = require('axios');
 
-const getAllPokemon = async (req, res) => {
+// Obtenemos los pokemons desde la api
+const pkmnApi = async () => {
     try {
-        const api = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=40')
-        const db = await Pokemon.findAll({include: Type})
-        const apiRes = api.data.results?.map(poke => axios.get(poke.url));
-        const pkmn = await axios.all(apiRes);
-        pkmn.map( async (pk) => {
-            return {
-                id: pk.data.id,
-                name: pk.data.name,
-                sprite: pk.data.sprites.other.dream_world.front_default 
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=40');    // Devuelve a los pokemon de la api
+        const subResponse = response.data.results?.map(pk => axios.get(pk.url));           // Realizamos otra petición a la url de cada pokemon 
+        const pokeRes = await axios.all(subResponse);
+
+        let pokemons = pokeRes.map(pk => pk.data);                                         // Devuelve los datos de cada pokemon
+        let pokeData = pokemons.map(pk => {                                                // Mapeamos los datos con la forma de nuestro modelo 
+            const pkmnData = {
+                id: pk.id,
+                name: pk.name,
+                sprite: pk.sprites.other.dream_world.front_default 
                     || "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/MissingNo.png/320px-MissingNo.png",
-                height: pk.data.height,
-                weight: pk.data.weight,
-                hp: pk.data.stats[0].base_stat,
-                attack: pk.data.stats[1].base_stat,
-                defense: pk.data.stats[2].base_stat,
-                esp_attack: pk.data.stats[3].base_stat,
-                esp_defense: pk.data.stats[4].base_stat,
-                speed: pk.data.stats[5].base_stat,
-                types: pk.data.types.length < 2? [{name: pk.types[0].type.name}] 
-                    : [{name: pk.types[0].type.name}, {name: pk.types[0].type.name}]
+                height: pk.height,
+                weight: pk.weight,
+                hp: pk.stats[0].base_stat,
+                attack: pk.stats[1].base_stat,
+                defense: pk.stats[2].base_stat,
+                esp_attack: pk.stats[3].base_stat,
+                esp_defense: pk.stats[4].base_stat,
+                speed: pk.stats[5].base_stat,
+                types: pk.types.length < 2? [{name: pk.types[0].type.name}] 
+                    : [{name: pk.types[0].type.name}, {name: pk.types[1].type.name}]
+            };
+            return pkmnData
+        })
+        console.log(pokeData)
+        return pokeData
+
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+};
+
+// Obtenemos los  pokemon desde la DB
+const pkmnDB = async () => {
+    try {
+        return await Pokemon.findAll({                         // Devuelve a todos los pokemon de la DB 
+            include: {
+                model: Type,                                   // Agrega la información de sus tipos
+                attibutes: ['name'],
             }
         })
-        if (api || db) {
-            let response = [...pkmn, db]
-            res.json(response)
-        }
-    } catch(e) {
-        console.error(e)
+
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+}
+
+// Mandamos todos los pokemon, tanto de Api como DB
+const getAllPokemon = async (req, res) => {
+    try {
+        const pokemonApi = await pkmnApi();                    // Recibe los pokemon de la Api
+        const pokemonDB = await pkmnDB();                      // Recibe los pokemon de la DB
+        let response = [...pokemonApi, ...pokemonDB];          // Los concatena para enviarlos juntos
+        res.json(response.flat());
+
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+}
+
+// Obtenemos los Pokemon buscando por su nombre
+const getPokemonByName = async (name) => {
+    try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)   // Nos devuelve los datos del pokemon que buscamos
+        const pkmnData = {                                                              // Formateamos la info a nuestro modelo
+            id: response.data.id,
+            name: response.data.name,
+            sprite: response.data.sprites.other.dream_world.front_default 
+            || "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/MissingNo.png/320px-MissingNo.png",
+            height: response.data.height,
+            weight: response.data.weight,
+            hp: response.data.stats[0].base_stat,
+            attack: response.data.stats[1].base_stat,
+            defense: response.data.stats[2].base_stat,
+            esp_attack: response.data.stats[3].base_stat,
+            esp_defense: response.data.stats[4].base_stat,
+            speed: response.data.stats[5].base_stat,
+            types: response.data.types.length < 2? [{name: response.data.types[0].type.name}] 
+            : [{name: response.data.types[0].type.name}, {name: response.data.types[1].type.name}]
+        };
+        
+        return pkmnData              // Enviamos los datos del pokemon ya formateados
+
+    } catch (e) {
+        console.log(e);
+        return e;
     }
 }
 
 module.exports = {
+    pkmnApi,
+    pkmnDB,
     getAllPokemon,
+    getPokemonByName,
 }
--- api/src/routes/pokemonRoute.js
+++ api/src/routes/pokemonRoute.js
@@ -1,18 +1,32 @@
 const { Router } = require("express");
 const { 
+    pkmnApi,
+    pkmnDB, 
     getAllPokemon, 
-    //getPokemonById, 
-    //getPokemonByName,
-    //postNewPokemon
+    getPokemonByName,
 } = require ('../controllers/pokemonController')
 
 const pokeRouter = Router();
 
 // Configurar los routers
 
-pokeRouter.get('/', getAllPokemon);
+pokeRouter.get('/', async (req, res) => {
+    try {
+        const { name } = req.query;
+        if(!name) {
+            return res.status(200).send(await getAllPokemon());
+        } else {
+            const poke = await getPokemonByName(name);
+            if(poke) {
+                return res.status(200).json(poke)
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send('Pokémon not found')
+    }
+});
 //pokeRouter.get('/:id', getPokemonById);
-//pokeRouter.get('/', getPokemonByName);
 //pokeRouter.post('/create', postNewPokemon)
 
 module.exports = pokeRouter;
